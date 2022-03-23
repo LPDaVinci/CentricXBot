f82447c140aa1bad8761ff4b4341669db90a1082
LPDaVinci <lpdavinci@users.noreply.github.com>
2022-03-23 17:20:47 +0100

    Changed Program Base and Adding Handlers

---
+++ src/Modules/Fun/Test.cs
@@ -0,0 +1,35 @@
+﻿using Discord;
+using Discord.Net;
+using Discord.WebSocket;
+using Discord.Commands;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+
+namespace CentricXBot.Modules.Fun
+{
+    // for commands to be available, and have the Context passed to them, we must inherit ModuleBase
+    public class Test : ModuleBase
+    {
+        [Command("test")]
+        public async Task TestComand()
+        {
+            // initialize empty string builder for reply
+            var sb = new StringBuilder();
+
+            // get user info from the Context
+            var user = Context.User;
+
+            // build out the reply
+            sb.AppendLine("TESTCOMMAND");
+
+            // send simple string reply
+            await ReplyAsync(sb.ToString());
+        }
+
+       
+    }
+}
