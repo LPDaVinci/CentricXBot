7dc8c3b656e8174b8f144effc837d02a642a633f
LPDaVinci <20724136+LPDaVinci@users.noreply.github.com>
2022-05-02 18:25:38 +0200

    Better clear command without blocking hopefully

--- CentricXBot/Modules/Fun/Clear.cs
+++ CentricXBot/Modules/Fun/Clear.cs
@@ -1,45 +1,59 @@
 ﻿using Discord;
 using Discord.Commands;
+using Discord.WebSocket;
 
 namespace CentricXBot.Modules.Fun
 {
     // for commands to be available, and have the Context passed to them, we must inherit ModuleBase
     public class Clear : ModuleBase<SocketCommandContext>
     {
+        private bool weekcheck = false;
         [Command("clear")]
-       public async Task ClearAsync(int amount)
+       public async Task ClearAsync(uint amount, ulong bottomMessageId = 0)
 {
-    if (amount <= 0)
-    {
-        IUserMessage m = await ReplyAsync("The amount of messages to remove must be positive.");
-        await Task.Delay(2000);
-        await m.DeleteAsync();
-        return;
-    }
-    var messages = await Context.Channel.GetMessagesAsync(Context.Message, Direction.Before, amount).FlattenAsync();
+            var context = Context;
+			var guild = context.Guild;
+            var channel = (Context.Channel as ITextChannel);
+             
+            var utcNow = DateTime.UtcNow.AddMinutes(1); //+1 min
 
-    var filteredMessages = messages.Where(x => (DateTimeOffset.UtcNow - x.Timestamp).TotalDays <= 14);
+            var messageList = new List<IMessage>();
 
-    // Get the total amount of messages.
-    var count = filteredMessages.Count();
+            	if (bottomMessageId != 0) {
+				amount--;
 
-    // Check if there are any messages to delete.
-    if (count == 0)
-    {
-        IUserMessage m = await ReplyAsync("Nothing to delete or Text is older than 14 Days");
-        await Task.Delay(2000);
-        await m.DeleteAsync();
-    }
-    else
-    {
-        await (Context.Channel as ITextChannel).DeleteMessagesAsync(filteredMessages);
-        IUserMessage m = await ReplyAsync($"Done. Removed {count} {(count > 1 ? "messages" : "message")}.");
-        await Task.Delay(2000);
-        await m.DeleteAsync();
-    }
-        await Context.Message.DeleteAsync();
-    
+				messageList.Add(await channel.GetMessageAsync(bottomMessageId));
+			}
 
+            List<ulong> msgToDel = new List<ulong>();
+
+			messageList.AddRange(
+				(await channel.GetMessagesAsync((int)amount + 1).FlattenAsync()).Where(m => {
+					if (m == null) {
+						return false;
+					}
+
+                    if ((utcNow - m.Timestamp.UtcDateTime).TotalDays >= 14) {
+                        weekcheck = true;
+                        return false;
+              
+					}
+
+					return true;
+				})
+			);
+            var count = messageList.Count() - 1;
+            if (weekcheck) {
+                msgToDel.Add((await ReplyAsync($":white_check_mark: I have deleted `{count} {(count > 1 ? "messages" : "message")}`!\n\n" +
+                ":negative_squared_cross_mark: Because of Discord limitations I can't delete messages past 2 weeks.\n\n" +
+                "If you want to clear a whole channel just right-click the channel then select 'Clone Channel'")).Id); //Send a msg, then add the msg ID to the list.
+            } else {
+                msgToDel.Add((await ReplyAsync($":white_check_mark: I have deleted `{count} {(count > 1 ? "messages" : "message")}`!")).Id); 
+            }            
+			await channel.DeleteMessagesAsync(messageList);             
+            await Task.Delay(1000);
+            await channel.DeleteMessagesAsync(msgToDel);
+            weekcheck = false;
 }
-}
+    }
 }
